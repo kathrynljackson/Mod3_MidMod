aa09ff9f39069b1f2f36339fbb14fad81c83773e
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-10-21 12:03:19 -0600

    Add Form

---
+++ src/Form/Form.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            name: '',
+            date: '',
+            time: '',
+            number: '',
+        }
+    }
+
+
+
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value })  
+    }
+    
+    makeReservation = (event) => {
+        event.preventDefault();
+        
+        const newReservation = {
+            id: Date.now(),
+            ...this.state
+        }
+
+        this.props.addReservation(newReservation);
+        this.clearAll();
+    }
+
+    clearAll = () => {
+        this.setState( { 
+            name: '',
+            date: '',
+            time: '',
+            number: '',
+        })
+    }
+
+
+    render() {
+        return (
+                <section className='user-input'>
+                    <input onChange={event => this.handleChange(event)} type='text' name='name' value ={this.state.name} placeholder='Name'></input>
+                    <input onChange={event => this.handleChange(event)} type='text' name='date' value ={this.state.date} placeholder='Date (mm/dd)'></input>
+                    <input onChange={event => this.handleChange(event)} type='text' name='time' value ={this.state.time} placeholder='Time'></input>
+                    <input onChange={event => this.handleChange(event)} type='text' name='number' value ={this.state.number} placeholder='Number of Guests'></input>
+                    <button className='make-reservation-button button' type='submit' onClick={event => this.makeReservation(event)}>Make Reservation</button>
+                </section>
+        )
+    }
+}
+
+export default Form;
+
