687c0fe87a1b2449d2bfec38b187e3e1ed20fa89
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-10-21 10:18:36 -0600

    Successfully fetch data, not yet displaying

--- src/Reservation/reservation.js
+++ src/Reservation/reservation.js
@@ -3,24 +3,35 @@ import { Component } from 'react';
 import { fetchReservations } from '../fetch/fetch-requests.js';
 
 class Reservation extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             reservations: [],
-        }
+        };
     }
 
 componentDidMount() {
     console.log('COMPONENT DID MOUNT IS RUNNING')
     fetchReservations()
-        .then(data => this.setState({ reservations: data }))
-        .catch(error => console.log(error))
+        //.then(data => this.setState({ reservations: data }))
+        //.catch(error => console.log(error))
 }
 
 render() {
     return (
         <main>
-            <section>
+            <section className='existing-reservations'>
+                {this.state.reservations.map(reservation => {
+                    return (
+                        <section className='each-reservation'>
+                        <p>{reservation.name}</p>
+                        <p>{reservation.date}</p>
+                        <p>{reservation.time}</p>
+                        <p>{reservation.number}</p>
+                        <button>Cancel</button>
+                        </section>
+                    )
+                })}
 
             </section>
         </main>
